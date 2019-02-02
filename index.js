bfad109315b9bb67f9b3d8884f12b341f75ef3ab
Semyon Fomin <taxnuke@gmail.com>
2019-02-02 20:08:42 +0300

    Improve output formatting

--- index.js
+++ index.js
@@ -34,14 +34,32 @@ Test.prototype.eqv = function (actual, expected, msg) {
 }
 
 Test.prototype.run = function () {
+  console.log('Woof! 🐕\n')
   console.log(this._desc)
   this._queue.forEach(function (assertion) {
-    if (assertion.exec()) {
-      console.log('\t👍')
-    } else {
+    if (!assertion.exec()) {
       console.log('\t👎', assertion.msg)
     }
   })
+
+  var total = this._queue.reduce(function (acc, c) {
+    if (c.state === states.passed) {
+      acc.passed++
+    } else if (c.state === states.failed) {
+      acc.failed++
+    } else {
+      acc.pending++
+    }
+
+    return acc
+  }, { passed: 0, failed: 0, pending: 0 })
+
+  console.log(
+    '\nTotal🐾\n\t%d 👍\n\t%d 👎\n\t%d ✋',
+    total.passed,
+    total.failed,
+    total.pending
+  )
 }
 
 function test(desc, cb) {
